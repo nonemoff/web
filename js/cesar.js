774d50b7719660d585390a178207d66c65e5a10b
Matvey Ushakoff <ushiz.rush@gmail.com>
2023-10-31 08:14:24 +0300

    made cesar hw 4

---
+++ js/cesar.js
@@ -0,0 +1,24 @@
+function cesar(str, shift, action) {
+    const alphabet = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
+    const alphabetLength = alphabet.length;
+
+    return str.split('').map(char => {
+        if (alphabet.includes(char)) {
+            const index = alphabet.indexOf(char);
+            if (action === 'encode') {
+                const encodedIndex = (index + shift) % alphabetLength;
+                return alphabet[encodedIndex];
+            } else if (action === 'decode') {
+                const decodedIndex = (index - shift + alphabetLength) % alphabetLength;
+                return alphabet[decodedIndex];
+            }
+        }
+        return char;
+    }).join('');
+}
+
+const messageToEncode = "эзтыхз фзъзъз";
+for (let i = 0; i < 31; i++){
+  const encodedMessage = cesar(messageToEncode, i, 'decode');
+  console.log(encodedMessage);
+}
